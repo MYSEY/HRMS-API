eae2827942c0f31dc7cb02d0b5d570970ef589f5
HovHong <hshong9666@gmail.com>
2025-06-03 15:42:14 +0700

    new function update password on login

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { validate } = require('express-validation');
-const { login, logout, register } = require('../controllers/auth.Controller');
+const { login, updatePassword, logout, register } = require('../controllers/auth.Controller');
 const { loginInRequest } = require('../validation/user');
 
 const router = express.Router(); 
 router.post('/login', validate(loginInRequest, {}, {}), login);
+router.post('/confirm', updatePassword);
 router.post('/logout', logout);
 router.post('/register', register);
 
