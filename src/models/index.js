c4b72310b3d3b5933eb300e89dc6ced9b37107d6
HovHong <hshong9666@gmail.com>
2025-06-25 14:34:51 +0700

    update leave

--- src/models/index.js
+++ src/models/index.js
@@ -66,15 +66,15 @@ db.MotorRentalDetail = require('./motor_rental_detail')(sequelize, DataTypes);
 db.LeaveRequest = require('./leave_request')(sequelize, DataTypes);
 db.LeaveType = require('./leave_type')(sequelize, DataTypes);
 db.DelegateLeave = require('./delegate_leave')(sequelize, DataTypes);
+db.LeaveAllocation = require('./leave_allocation')(sequelize, DataTypes);
 // LeaveRequest model
 db.LeaveRequest.belongsTo(db.LeaveType, { foreignKey: 'leave_type_id' });
 db.LeaveRequest.belongsTo(db.user, { foreignKey: 'employee_id' });
 db.LeaveRequest.belongsTo(db.user, { as: 'HandoverStaff', foreignKey: 'handover_staff_id' });
 db.LeaveRequest.belongsTo(db.DelegateLeave, { as: 'DelegateLeave', foreignKey: 'employee_id', targetKey: 'requester_id'});
+// db.LeaveRequest.belongsTo(db.LeaveAllocation, { as: 'LeaveAllocation', foreignKey: 'employee_id', targetKey: 'employee_id'});
 // LeaveType model
 db.LeaveType.hasMany(db.LeaveRequest, { foreignKey: 'leave_type_id' });
-
-db.LeaveAllocation = require('./leave_allocation')(sequelize, DataTypes);
 
 db.Training = require('./training')(sequelize, DataTypes);
 
